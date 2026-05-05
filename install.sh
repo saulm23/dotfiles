682824e4d4a48a6969a3daa4bfc089f4e2c63b0c
saulm23 <marcasaul00@gmail.com>
2026-05-05 18:20:47 -0400

    install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+rm -rf ~/.config/*
+mkdir -p ~/.config/kitty
+mkdir -p ~/.config/picom
+mkdir -p ~/.config/i3
+mkdir -p ~/.config/i3status
+
+ln -s -T ~/dotfiles/i3/config ~/.config/i3/config
+ln -s -T ~/dotfiles/i3/i3status.conf ~/.config/i3status/config
+ln -s -T ~/dotfiles/kitty/kitty.conf ~/.config/kitty/kitty.conf
+ln -s -T ~/dotfiles/kitty/current-theme.conf ~/.config/kitty/current-theme.conf
+ln -s -T ~/dotfiles/picom/picom.conf ~/.config/picom/picom.conf
+
+
