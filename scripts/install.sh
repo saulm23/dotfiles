eaf61fba3c7b2c2138b63fc55e35a50ba830187f
saulm23 <marcasaul00@gmail.com>
2026-05-25 11:53:10 -0400

    remove kitty

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-rm -rf ~/.config/*
-mkdir -p ~/.config/kitty
+#rm -rf ~/.config/*
+
+rm -rf ~/.config/alacritty/*
+rm -rf ~/.config/picom/*
+rm -rf ~/.config/i3/*
+rm -rf ~/.config/i3status/*
+rm -rf ~/.config/tmux/*
+
 mkdir -p ~/.config/alacritty
 mkdir -p ~/.config/picom
 mkdir -p ~/.config/i3
 mkdir -p ~/.config/i3status
 mkdir -p ~/.config/tmux
+mkdir -p ~/screenshots
 
 ln -s -T ~/dotfiles/i3/config ~/.config/i3/config
 ln -s -T ~/dotfiles/i3/i3status.conf ~/.config/i3status/config
-ln -s -T ~/dotfiles/kitty/kitty.conf ~/.config/kitty/kitty.conf
 ln -s -T ~/dotfiles/alacritty/alacritty.toml  ~/.config/alacritty/alacritty.toml
-ln -s -T ~/dotfiles/kitty/current-theme.conf ~/.config/kitty/current-theme.conf
 ln -s -T ~/dotfiles/picom/picom.conf ~/.config/picom/picom.conf
 
 ln -s -T ~/dotfiles/tmux/tmux.conf ~/.config/tmux/tmux.conf
