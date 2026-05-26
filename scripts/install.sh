599c3221d8aa98b6e164b2400577a33e696f9ff0
saulm23 <marcasaul00@gmail.com>
2026-05-26 09:10:33 -0400

    add nvim

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-#rm -rf ~/.config/*
 
 rm -rf ~/.config/alacritty/*
 rm -rf ~/.config/picom/*
 rm -rf ~/.config/i3/*
 rm -rf ~/.config/i3status/*
 rm -rf ~/.config/tmux/*
+rm -rf ~/.config/nvim/*
 
 mkdir -p ~/.config/alacritty
 mkdir -p ~/.config/picom
 mkdir -p ~/.config/i3
 mkdir -p ~/.config/i3status
 mkdir -p ~/.config/tmux
+mkdir -p ~/.config/nvim
 mkdir -p ~/screenshots
 
 ln -s -T ~/dotfiles/i3/config ~/.config/i3/config
@@ -19,12 +20,14 @@ ln -s -T ~/dotfiles/i3/i3status.conf ~/.config/i3status/config
 ln -s -T ~/dotfiles/alacritty/alacritty.toml  ~/.config/alacritty/alacritty.toml
 ln -s -T ~/dotfiles/picom/picom.conf ~/.config/picom/picom.conf
 ln -s -T ~/dotfiles/tmux/tmux.conf ~/.config/tmux/tmux.conf
-
+ln -s -T ~/dotfiles/nvim/init.lua ~/.config/nvim/init.lua
 
 rm ~/.vimrc
 ln -s -T ~/dotfiles/vim ~/.vimrc
 
 rm ~/.bashrc
 ln -s -T ~/dotfiles/bash ~/.bashrc
+
 echo exec i3 > ~/.xinitrc
+
 source ~/.bashrc
