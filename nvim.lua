1aecf67765f84cc2d0159cb7fe8cc6e18670db51
Saulll <173097323+saulm23@users.noreply.github.com>
2026-05-26 10:52:15 -0400

    Create nvim.lua

---
+++ nvim.lua
@@ -0,0 +1,44 @@
+-- Numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Hide bottom/status bar
+vim.opt.laststatus = 0
+vim.opt.showmode = true
+
+-- UI
+--vim.opt.cursorline = true
+--vim.opt.signcolumn = "no"
+
+-- Tabs
+vim.opt.smartindent = false
+
+-- Search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+
+
+-- Pure black & white theme
+vim.cmd([[
+  highlight Normal guibg=#000000 guifg=#FFFFFF
+  highlight CursorLine guibg=#111111
+  highlight LineNr guifg=#666666
+  highlight CursorLineNr guifg=#FFFFFF gui=bold
+
+  highlight Comment guifg=#888888 gui=italic
+  highlight Constant guifg=#FFFFFF
+  highlight String guifg=#DDDDDD
+  highlight Identifier guifg=#FFFFFF
+  highlight Function guifg=#FFFFFF gui=bold
+  highlight Statement guifg=#FFFFFF gui=bold
+  highlight Keyword guifg=#FFFFFF gui=bold
+  highlight Type guifg=#FFFFFF
+  highlight Special guifg=#FFFFFF
+
+  highlight Visual guibg=#222222
+  highlight Search guibg=#FFFFFF guifg=#000000
+
+  highlight StatusLine guibg=#000000 guifg=#000000
+  highlight VertSplit guifg=#222222
+]])
